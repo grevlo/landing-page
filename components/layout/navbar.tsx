d2ddd6e1fab9e06ee3223306fbff865cb267c726
Dom Howarth <dom@grevlo.com>
2026-04-03 14:52:51 +0100

    Use PNG logo in navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -12,7 +12,6 @@ import {
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import Image from "next/image";
 
 interface RouteProps {
   href: string;
@@ -30,16 +29,11 @@ export const Navbar = () => {
   return (
     <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="flex items-center gap-2 px-2">
-        <Image
-          src="/logo-horizontal.svg"
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/logo-horizontal.png"
           alt="Grevlo"
-          width={160}
-          height={48}
-          priority
-          style={{
-            filter:
-              "brightness(0) saturate(100%) invert(19%) sepia(45%) saturate(1200%) hue-rotate(200deg) brightness(95%) contrast(95%)",
-          }}
+          style={{ height: "36px", width: "auto" }}
         />
       </Link>
 
@@ -60,15 +54,11 @@ export const Navbar = () => {
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle>
                   <Link href="/" onClick={() => setIsOpen(false)}>
-                    <Image
-                      src="/logo-horizontal.svg"
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src="/logo-horizontal.png"
                       alt="Grevlo"
-                      width={160}
-                      height={48}
-                      style={{
-                        filter:
-                          "brightness(0) saturate(100%) invert(19%) sepia(45%) saturate(1200%) hue-rotate(200deg) brightness(95%) contrast(95%)",
-                      }}
+                      style={{ height: "36px", width: "auto" }}
                     />
                   </Link>
                 </SheetTitle>

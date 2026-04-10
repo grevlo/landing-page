ceaaa6027f5bcf8ee5079db5295a15c787408b78
Dom Howarth <dom@grevlo.com>
2026-04-11 00:33:32 +0100

    Add llms.txt and allow AI crawlers in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,28 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Claude-Web',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'GoogleOther',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://grevlo.com/sitemap.xml',
   }
 }
